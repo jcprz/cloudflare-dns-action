cd2a916e382135b0901337bdf84844199ed51e1b
Julio Perez Abreu <jcesarpabreu@gmail.com>
2023-07-18 12:10:36 -0400

    commiting changes

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # cloudflare-management-action
+
+This GitHub Action is designed to manage DNS records using the Cloudflare API. You can use it to check if a DNS record exists, create a new DNS record, or delete an existing DNS record.
+
+## Inputs
+
+### `auth-token`
+
+**Required.** This is your authorization token for the Cloudflare API.
+
+### `zone-id`
+
+**Required.** This is the ID of the DNS zone in Cloudflare where the record resides or will be created.
+
+### `record-type`
+
+**Required.** The type of DNS record you wish to manage (e.g., `A`, `CNAME`, `TXT`, etc.).
+
+### `record-name`
+
+**Required.** The name of the DNS record you wish to manage.
+
+### `record-content`
+
+**Required.** The content of the DNS record you wish to manage.
+
+### `action`
+
+**Required.** The action you wish to perform on the DNS record. This can be `check`, `create`, or `delete`.
+
+### `create-if-not-exists`
+
+This is optional. If set to `true` and the `action` is set to `check`, the action will automatically create the record if it does not exist. The default is `false`.
+
+## Example Usage
+
+This is an example of how to use this action in a GitHub Workflow:
+
+```yaml
+- name: Manage DNS records
+  uses: your-org/manage-dns-action@v1
+  with:
+    auth-token: ${{ secrets.CLOUDFLARE_API_TOKEN }}
+    zone-id: ${{ secrets.CLOUDFLARE_ZONE_ID }}
+    record-type: CNAME
+    record-name: my.example.com
+    record-content: example.com
+    action: create
+    create-if-not-exists: true
+```
+
+In this example, the action will check if a CNAME record for example.com exists. If the record does not exist and create-if-not-exists is set to true, it will create the record.
--- run.sh
+++ run.sh
@@ -1,42 +1,79 @@
 #!/bin/bash
 set -e
 
-case "$ACTION" in
-    check)
-        response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=$RECORD_TYPE&name=$RECORD_NAME" \
-            -H "Authorization: Bearer $AUTH_TOKEN" \
-            -H "Content-Type: application/json" | jq -r '.result[]?.name')
+auth_token=$1
+zone_id=$2
+record_type=$3
+record_name=$4
+record_content=$5
+action=$6
+create_if_not_exists=$7
+ttl=$8
+comment=$9
+proxied=${10}
 
-        if [ "$response" == "$RECORD_NAME" ]; then
-            echo "DNS Record exists"
-            exit 0
-        else
-            echo "DNS Record does not exist"
-            if [ "$CREATE_IF_NOT_EXISTS" == "true" ]; then
-                bash $0 create
-            else
-                exit 1
-            fi
-        fi
-        ;;
+# Check or validate DNS record
+if [[ $action == "validate" ]]; then
+  response=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?type=${record_type}&name=${record_name}" \
+      -H "Authorization: Bearer ${auth_token}" \
+      -H "Content-Type: application/json" | jq -r '.result[0].id')
 
-    create)
-        curl -X POST "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records" \
-            -H "Authorization: Bearer $AUTH_TOKEN" \
-            -H "Content-Type: application/json" \
-            --data \
-                "{
-                \"type\": \"$RECORD_TYPE\",
-                \"name\": \"$RECORD_NAME\",
-                \"content\": \"$RECORD_CONTENT\",
-                \"ttl\":1,
-                \"proxied\":false
-                }"
-        ;;
+  if [ "$response" != "null" ]; then
+    echo "DNS Record exists"
+  else
+    echo "DNS Record does not exist"
+    if [[ $create_if_not_exists == "true" ]]; then
+      action="create"
+    else
+      exit 1
+    fi
+  fi
+fi
 
-    delete)
-        curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_NAME" \
-            -H "Authorization: Bearer $AUTH_TOKEN" \
-            -H "Content-Type: application/json"
-        ;;
-esac
+# Create DNS record
+if [[ $action == "create" ]]; then
+  echo "Creating DNS record"
+  curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records" \
+    -H "Authorization: Bearer ${auth_token}" \
+    -H "Content-Type: application/json" \
+    --data \
+      '{
+        "type": "'"$record_type"'",
+        "name": "'"$record_name"'",
+        "content": "'"$record_content"'",
+        "ttl": '"$ttl"',
+        "proxied": '"$proxied"',
+        "comment": "'"$comment"'"
+      }'
+fi
+
+# Update DNS record
+if [[ $action == "update" ]]; then
+  echo "Updating DNS record"
+  record_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?type=${record_type}&name=${record_name}" \
+      -H "Authorization: Bearer ${auth_token}" \
+      -H "Content-Type: application/json" | jq -r '.result[0].id')
+  curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
+    -H "Authorization: Bearer ${auth_token}" \
+    -H "Content-Type: application/json" \
+    --data \
+      '{
+        "type": "'"$record_type"'",
+        "name": "'"$record_name"'",
+        "content": "'"$record_content"'",
+        "ttl": '"$ttl"',
+        "proxied": '"$proxied"',
+        "comment": "'"$comment"'"
+      }'
+fi
+
+# Delete DNS record
+if [[ $action == "delete" ]]; then
+  echo "Deleting DNS record"
+  record_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?type=${record_type}&name=${record_name}" \
+      -H "Authorization: Bearer ${auth_token}" \
+      -H "Content-Type: application/json" | jq -r '.result[0].id')
+  curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
+    -H "Authorization: Bearer ${auth_token}" \
+    -H "Content-Type: application/json"
+fi
